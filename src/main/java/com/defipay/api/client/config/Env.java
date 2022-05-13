279aba67d79f193c52f0ea0923d821dfebb90d20
harlan <438136451@qq.com>
2022-05-13 16:28:42 +0800

    环境变更

--- src/main/java/com/defipay/api/client/config/Env.java
+++ src/main/java/com/defipay/api/client/config/Env.java
@@ -3,7 +3,7 @@ package com.defipay.api.client.config;
 public class Env {
     //todo
     public static Env SANDBOX = new Env("https://api-test.defipay.biz/", "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4");
-    public static Env PROD = new Env("https://api-test.defipay.biz/", "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4");
+    public static Env PROD = new Env("https://api.defipay.biz/", "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4");
 
     public String host;
     public String defipayPub;
