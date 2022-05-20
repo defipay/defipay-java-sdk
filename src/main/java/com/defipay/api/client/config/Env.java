bda6d248b6508516845f7da717f896e2552fee24
harlan <438136451@qq.com>
2022-05-20 18:00:52 +0800

    环境 公钥变更

--- src/main/java/com/defipay/api/client/config/Env.java
+++ src/main/java/com/defipay/api/client/config/Env.java
@@ -2,8 +2,8 @@ package com.defipay.api.client.config;
 
 public class Env {
     //todo
-    public static Env SANDBOX = new Env("https://api-test.defipay.biz/", "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4");
-    public static Env PROD = new Env("https://api.defipay.biz/", "0314c127b69c03545b49aaf365c56575ce9b1640cad8eb6ca47c34c8322fe9f4d4");
+    public static Env SANDBOX = new Env("https://api-test.defipay.biz/", "02adb46f0c10b5ec51d0df2183a812fdf7b330ef2c948e36cdb479f1af73a22753");
+    public static Env PROD = new Env("https://api.defipay.biz/", "02adb46f0c10b5ec51d0df2183a812fdf7b330ef2c948e36cdb479f1af73a22753");
 
     public String host;
     public String defipayPub;
